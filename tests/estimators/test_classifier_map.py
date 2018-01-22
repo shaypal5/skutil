ae75bcab6c83d5d29e03206042fed0e7de15eba0
ShayPalachy <shay@zencity.io>
2018-01-22 15:50:50 +0000

    classifier_cls_by_name fully tested

---
+++ tests/estimators/test_classifier_map.py
@@ -0,0 +1,18 @@
+"""Test classifier_cls_by_name."""
+
+
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+
+
+from skutil.estimators import classifier_cls_by_name
+
+
+def test_base():
+    assert classifier_cls_by_name('LogisticRegression') == LogisticRegression
+    assert classifier_cls_by_name('logisticregression') == LogisticRegression
+    assert classifier_cls_by_name('logreg') == LogisticRegression
+    assert classifier_cls_by_name('SVC') == SVC
+    assert classifier_cls_by_name('svc') == SVC
+    assert classifier_cls_by_name('SVM') == SVC
+    assert classifier_cls_by_name('svm') == SVC
