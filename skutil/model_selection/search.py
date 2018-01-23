7349323bf4f5bf9043c27b795a97ccb3b5c0b482
ShayPalachy <shay@zencity.io>
2018-01-23 12:03:14 +0000

    COnstraineParameterGrid doctest is now deterministic

--- skutil/model_selection/search.py
+++ skutil/model_selection/search.py
@@ -25,12 +25,12 @@ class ConstrainedParameterGrid(ParameterGrid):
     -------
     >>> param = {'a': [1, 2], 'b': [3, 4, 5]}
     >>> grid = ConstrainedParameterGrid(param, [{'a': 1, 'b': 4}])
-    >>> for params in grid: print(params)
-    {'a': 1, 'b': 3}
-    {'a': 1, 'b': 5}
-    {'a': 2, 'b': 3}
-    {'a': 2, 'b': 4}
-    {'a': 2, 'b': 5}
+    >>> for params in grid: print(sorted(params.items()))
+    [('a', 1), ('b', 3)]
+    [('a', 1), ('b', 5)]
+    [('a', 2), ('b', 3)]
+    [('a', 2), ('b', 4)]
+    [('a', 2), ('b', 5)]
     """
 
     def __init__(self, param_grid, bad_comb=None):
