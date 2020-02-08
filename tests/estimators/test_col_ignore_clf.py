c7d00da911e066f63b86d31c5da4954875ad8544
Shay Palachy <shaypal5@gmail.com>
2020-02-08 14:08:58 +0200

    removed needles test for sparse dataframes

--- tests/estimators/test_col_ignore_clf.py
+++ tests/estimators/test_col_ignore_clf.py
@@ -80,28 +80,3 @@ def test_obj_col_ignoring_clf():
         assert isinstance(proba, float)
         assert proba <= 1
         assert proba >= 0
-
-
-def test_obj_col_ignoring_clf_w_sparse():
-    df = pd.DataFrame(data=BASE_DATA, columns=['x1', 'x2', 'x3', 'y'])
-    df = df.to_sparse()
-    X, y = x_y_by_col_lbl(df=df, y_col_lbl='y')
-    y = y.values
-
-    clf = SVC(probability=True, gamma='auto')
-    ignore_clf = ObjColIgnoringClassifier(clf=clf)
-    fitted_ignore_clf = ignore_clf.fit(X, y, validate=False, sparse=True)
-    assert fitted_ignore_clf == ignore_clf
-
-    test_data = [[1, '233', 30]]
-    test_df = pd.DataFrame(test_data, columns=['x1', 'x2', 'x3'])
-    res = fitted_ignore_clf.predict(test_df)
-    assert len(res) == 1
-
-    res2 = fitted_ignore_clf.predict_proba(test_df)
-    assert len(res2) == 1
-    assert len(res2[0] == 2)
-    for proba in res2[0]:
-        assert isinstance(proba, float)
-        assert proba <= 1
-        assert proba >= 0
